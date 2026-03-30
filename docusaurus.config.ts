862d0e7f86533a9a29a9efacf268bb53b0d9187a
kevin-malik <kevin.malik@sg-edts.com>
2026-03-30 13:46:39 +0700

    chore: update baseUrl to root path in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://kevinmf.my.id',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kevin-dev-journal/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
